bc5f4439734c773a9c68b161cdd573dac7aba4c6
Erth17 <16hjames@gmail.com>
2026-07-28 14:07:42 +0100

    /users username, password, age and card verification

--- Main.py
+++ Main.py
@@ -11,6 +11,16 @@ app = Flask(__name__)
 # Temporary storage
 users = []
 
+def validate_username(username):
+    # alphanumeric, no spaces
+    return re.fullmatch(r"[A-Za-z0-9]+", username) is not None
+
+def validate_password(password):
+    # min length 8, at least one upper case letter & number
+    if len(password) <= 8 or not re.search(r"[A-Z]", password) or not re.search(r"\d", password):
+        return False
+    return True
+
 def validate_email(email):
     pattern = r"^[^@\s]+@[^@\s]+\.[^@\s]+$"
     return re.match(pattern, email) is not None
@@ -21,6 +31,20 @@ def validate_date(date):
         return True
     except ValueError:
         return False
+
+def calculate_age(date_of_birth):
+    dob = datetime.strptime(date_of_birth, "%Y-%m-%d").date()
+    today = datetime.today().date()
+
+    age = today.year - dob.year
+    if (today.month, today.day) < (dob.month, dob.day):
+        age -= 1
+
+    return age
+
+def validate_card_number(card_number):
+    # If given should have 16 digits.
+    return re.fullmatch(r"\d{16}", card_number) is not None
 
 @app.route('/')
 def home():
@@ -77,22 +101,57 @@ def register_users():
     password = data['password']
     email = data['email']
     dob = data['DoB']
-    card_number = data['card_number']
+    card_number = data.get('card_number')
+
+    if not validate_username(username):
+        # Fails to satisfy valid username format
+        # return HTTP Status code: 400
+        return jsonify({
+            'error': "Username must only contain alphanumeric characters."
+        }), 400
+
+    if not validate_password(password):
+        # Fails to satisfy valid password format
+        # return HTTP Status code: 400
+        return jsonify({
+            'error': "Password must be longer than 8 characters and contain" +
+                     " at least one uppercase letter and one number."
+        }), 400
 
     if not validate_email(email):
         # Fails to satisfy valid email format
         # return HTTP Status code: 400
-        return jsonify({"error": f"Invalid email: {email}"}), 400
+        return jsonify({
+            'error': f"Invalid email: {email}"
+        }), 400
 
     if not validate_date(dob):
         # Fails to satisfy valid ISO 8601 format
         # return HTTP Status code: 400
-        return jsonify({"error": f"Invalid date format: {dob}"}), 400
+        return jsonify({
+            'error': "Please enter date of birth in YYYY-MM-DD format."
+        }), 400
+
+    if calculate_age(dob) < 18:
+        # Reject requests if the user is under the age of 18
+        # return HTTP Status code: 403
+        return jsonify({
+            'error': "You must be at least 18 years old to register."
+        }), 403
+
+    if card_number is not None and not validate_card_number(card_number):
+        # Fails to satisfy valid card number length
+        # return HTTP Status code: 400
+        return jsonify({
+            'error': "Card number should have 16 digits."
+        }), 400
 
     if any(user["username"] == username for user in users):
-        # If the username has already been used
-        # reject the request and return HTTP Status code: 409
-        return jsonify({"error": f"Username already exists"}), 409
+        # If the username has already been used reject the request
+        # return HTTP Status code: 409
+        return jsonify({
+            "error": f"Username already exists"
+        }), 409
 
     user = {
         "username": username,
