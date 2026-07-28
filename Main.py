4a18521a6d1e5131f842d101ccba66c4e7b1988f
Erth17 <16hjames@gmail.com>
2026-07-28 13:10:30 +0100

    /users additional verification and success

--- Main.py
+++ Main.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 #POST
 #PUT
 #DELETE
+
+# Temporary storage
+users = []
+
+def validate_email(email):
+    pass
+
+def validate_date(date):
+    pass
+
 @app.route('/')
 def home():
     return "Home"
@@ -57,6 +67,39 @@ def register_users():
         # If the request body fails to satisfy any of the basic validation checks
         # return HTTP Status code: 400
         return jsonify({"error": f"Missing fields: {', '.join(missing)}"}), 400
+
+    username = data['username']
+    password = data['password']
+    email = data['email']
+    dob = data['DoB']
+    card_number = data['card_number']
+
+    if not validate_email(email):
+        pass
+
+    if not validate_date(dob):
+        pass
+
+    if any(user["username"] == username for user in users):
+        # If the username has already been used
+        # reject the request and return HTTP Status code: 409
+        return jsonify({"error": f"Username already exists"}), 409
+
+    user = {
+        "username": username,
+        "email": email,
+        "password": password, # Obviously hashed
+        "dob": dob,
+        "card_number": card_number, # Obviously hashed
+    }
+
+    users.append(user)
+
+    return jsonify({
+        # A successful action should return HTTP Status code: 201
+        "message": "User created",
+        "user_id": user["id"],
+    }), 201
 
 
 if __name__ == '__main__':
